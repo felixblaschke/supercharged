1c062e90fdf971d8f353ea2014177b27bd7b1cd2
Felix Blaschke <felix@felix-blaschke.de>
2020-07-16 20:02:32 +0200

    fix code lints

--- test/int/tweento_test.dart
+++ test/int/tweento_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/animation.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:supercharged/supercharged.dart';
 
--- test/iterable/count_test.dart
+++ test/iterable/count_test.dart
@@ -8,7 +8,9 @@ void main() {
     expect([1, 2, 3, 13, 14, 15].count(), equals(6));
     expect([1, 2, 3, 13, 14, 15].count((n) => n > 9), equals(3));
     expect([1, 2, 3, 13, 14, 15].count((n) => n % 2 == 0), equals(2));
+    // ignore: equal_elements_in_set
     expect({1, 2, 3, 1, 2, 3}.count(), equals(3));
+    // ignore: equal_elements_in_set
     expect({1, 2, 3, 1, 2, 3}.count((n) => n % 2 == 0), equals(1));
   });
 }
