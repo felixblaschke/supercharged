15d0759962a5e22b56353f2ad1ceee0446e528a2
Jaspreet Singh <mail@jas.bio>
2020-08-01 14:01:36 +0530

    Added Generic type double and fixed typos

--- lib/animation/animation.dart
+++ lib/animation/animation.dart
@@ -1,30 +1,29 @@
 part of supercharged;
 
 /// Supercharged extension on [Animation] objects.
-extension Animation_ on Animation {
-  /// Returns an [Animation] after appling [CurveTween] based on begin and end.
+extension Animation_ on Animation<double> {
+  /// Returns an [Animation] after applying [CurveTween] based on begin and end.
   /// Optionally can passe [Curve] as well by pass curve object at last parameter.
   ///
   /// Example:
   /// ```dart
-  /// controller.interval(0.0, 1.0,curve:Curves.elasticOut)
+  /// controller.interval(0.0, 1.0, curve:Curves.elasticOut)
   /// ```
-  Animation interval(double begin, double end, {Curve curve}) {
-    // Value Guard
+  Animation<double> interval(double begin, double end, {Curve curve}) {
     assert(begin >= 0 && end <= 1);
     return drive(CurveTween(
       curve: Interval(begin, end, curve: curve ?? Curves.linear),
     ));
   }
 
-  /// Returns an [Animation] after appling [CurveTween]. You can just pass curve
+  /// Returns an [Animation] after applying [CurveTween]. You can just pass curve
   /// object.
   ///
   /// Example:
   /// ```dart
-  /// controller.interval(0.0, 0.5).curve(Curves.elasticOut)
+  /// controller.interval(0.0, 0.5).curve(Curves.easeInOut)
   /// ```
-  Animation curve(Curve curve) {
+  Animation<double> curve(Curve curve) {
     return drive(CurveTween(curve: curve));
   }
 }
