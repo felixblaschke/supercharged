cc646d676da86a389dcb88ce579d4d5e71e4539d
Jaspreet Singh <mail@jas.bio>
2020-08-01 14:27:19 +0530

    Refactor animation test

--- test/animation/animation_test.dart
+++ test/animation/animation_test.dart
@@ -4,64 +4,55 @@ import 'package:supercharged/supercharged.dart';
 
 void main() {
   test("animation interval and curve", () {
-    test1() {
-      final controller = AnimationController(
-        vsync: const TestVSync(),
-      );
-      final compare = compareAnimation(
-        parent: controller,
-        actual: controller.interval(0.3, 0.7),
-        matcher: controller.drive(CurveTween(curve: Interval(0.3, 0.7))),
-      );
-      controller.dispose();
-      return compare;
-    }
-
-    test2() {
-      final controller = AnimationController(
-        vsync: const TestVSync(),
-      );
-
-      final compare = compareAnimation(
-        parent: controller,
-        actual: controller.interval(0.3, 0.7, curve: Curves.easeInOut),
-        matcher: controller.drive(
+    expect(
+      compareAnimation(
+        actual: (controller) => controller.interval(0.3, 0.7),
+        matcher: (controller) =>
+            controller.drive(CurveTween(curve: Interval(0.3, 0.7))),
+      ),
+      true,
+    );
+    expect(
+      compareAnimation(
+        actual: (controller) =>
+            controller.interval(0.3, 0.7, curve: Curves.easeInOut),
+        matcher: (controller) => controller.drive(
             CurveTween(curve: Interval(0.3, 0.7, curve: Curves.easeInOut))),
-      );
-      controller.dispose();
-      return compare;
-    }
+      ),
+      true,
+    );
 
-    test3() {
-      final controller = AnimationController(
-        vsync: const TestVSync(),
-      );
-
-      final compare = compareAnimation(
-        parent: controller,
-        actual: controller.curve(Curves.easeOut),
-        matcher: controller.drive(CurveTween(curve: Curves.easeOut)),
-      );
-      controller.dispose();
-      return compare;
-    }
-
-    expect(test1(), true);
-    expect(test2(), true);
-    expect(test3(), true);
+    expect(
+      compareAnimation(
+        actual: (controller) => controller.curve(Curves.easeInOut),
+        matcher: (controller) =>
+            controller.drive(CurveTween(curve: Curves.easeInOut)),
+      ),
+      true,
+    );
   });
 }
 
 compareAnimation({
-  AnimationController parent,
-  Animation actual,
-  Animation matcher,
+  Animation<double> Function(AnimationController) actual,
+  Animation<double> Function(AnimationController) matcher,
 }) {
+  final controller = AnimationController(
+    vsync: const TestVSync(),
+  );
+
+  final _actual = actual(controller);
+  final _matcher = matcher(controller);
+
   double v = 0.0;
   while (v <= 1) {
     v += 0.01;
-    parent.value = v;
-    if (actual.value != matcher.value) return false;
+    controller.value = v;
+    if (_actual.value != _matcher.value) {
+      controller.dispose();
+      return false;
+    }
   }
+  controller.dispose();
   return true;
 }
