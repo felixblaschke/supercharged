8e9458fa452b3d62f2c0cd1b47704700ff1535f4
Felix Blaschke <felix@felix-blaschke.de>
2020-08-01 09:34:39 +0200

    tagging more flutter dependant lines #2

--- dartify/dartify.dart
+++ dartify/dartify.dart
@@ -5,17 +5,17 @@ import 'file_editor.dart';
 void main() async {
   FileEditor("pubspec.yaml")
     ..replaceLine(RegExp(r"^name:"), "name: supercharged_for_dart")
+    ..removeTaggedLines("flutter")
     ..save();
 
   Directory("test").deleteSync(recursive: true);
 
   Directory("lib").listSync(recursive: true).forEach((file) {
-    if (file.path.endsWith(".dart")) {
       FileEditor(file.path)
         ..removeTaggedLines("flutter")
         ..save();
-    }
   });
+
 
 
 }
