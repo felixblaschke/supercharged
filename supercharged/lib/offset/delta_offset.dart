800e9b29ca99ec1305906c0e1d3fdcc44f8629e6
Jop Middelkamp <middelkamp@live.nl>
2021-12-06 21:10:43 +0100

    DeltaOffset documentation correction

--- supercharged/lib/offset/delta_offset.dart
+++ supercharged/lib/offset/delta_offset.dart
@@ -2,22 +2,22 @@ part of supercharged;
 
 /// Supercharged helper to create an [Offset] based on directional deltas.
 class DeltaOffset extends Offset {
-  /// Creates a [DeltaOffset] based on right  deltas.
+  /// Creates a [DeltaOffset] based on right delta.
   const DeltaOffset.right(
     double right,
   ) : super(right, 0.0);
 
-  /// Creates a [DeltaOffset] based on top  deltas.
+  /// Creates a [DeltaOffset] based on top delta.
   const DeltaOffset.top(
     double top,
   ) : super(0.0, top * -1);
 
-  /// Creates a [DeltaOffset] based on left  deltas.
+  /// Creates a [DeltaOffset] based on left delta.
   const DeltaOffset.left(
     double left,
   ) : super(left * -1, 0.0);
 
-  /// Creates a [DeltaOffset] based on bottom  deltas.
+  /// Creates a [DeltaOffset] based on bottom delta.
   const DeltaOffset.bottom(
     double bottom,
   ) : super(0.0, bottom);
