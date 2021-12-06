f00092289b1207e402669dc820507c8ef1892f1a
Jop Middelkamp <middelkamp@live.nl>
2021-12-06 21:07:28 +0100

    Added top, botom, left, right to the DeltaOffset

--- supercharged/lib/offset/delta_offset.dart
+++ supercharged/lib/offset/delta_offset.dart
@@ -2,6 +2,26 @@ part of supercharged;
 
 /// Supercharged helper to create an [Offset] based on directional deltas.
 class DeltaOffset extends Offset {
+  /// Creates a [DeltaOffset] based on right  deltas.
+  const DeltaOffset.right(
+    double right,
+  ) : super(right, 0.0);
+
+  /// Creates a [DeltaOffset] based on top  deltas.
+  const DeltaOffset.top(
+    double top,
+  ) : super(0.0, top * -1);
+
+  /// Creates a [DeltaOffset] based on left  deltas.
+  const DeltaOffset.left(
+    double left,
+  ) : super(left * -1, 0.0);
+
+  /// Creates a [DeltaOffset] based on bottom  deltas.
+  const DeltaOffset.bottom(
+    double bottom,
+  ) : super(0.0, bottom);
+
   /// Creates a [DeltaOffset] based on right and top deltas.
   const DeltaOffset.rightTop(
     double right,
--- supercharged/test/offset/delta_offset_test.dart
+++ supercharged/test/offset/delta_offset_test.dart
@@ -2,6 +2,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:supercharged/supercharged.dart';
 
 void main() {
+  test('Construct by left constructor', () {
+    const offset = DeltaOffset.left(5);
+    expect(offset.dx, -5);
+    expect(offset.dy, 0);
+  });
+  test('Construct by top constructor', () {
+    const offset = DeltaOffset.top(5);
+    expect(offset.dx, 0);
+    expect(offset.dy, -5);
+  });
+  test('Construct by right constructor', () {
+    const offset = DeltaOffset.right(5);
+    expect(offset.dx, 5);
+    expect(offset.dy, 0);
+  });
+  test('Construct by bottom constructor', () {
+    const offset = DeltaOffset.bottom(5);
+    expect(offset.dx, 0);
+    expect(offset.dy, 5);
+  });
   test('Construct by leftBottom constructor', () {
     const offset = DeltaOffset.leftBottom(5, 5);
     expect(offset.dx, -5);
