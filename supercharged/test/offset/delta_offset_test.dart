ef1a2ba73588e0e875037955ae239b12bbc74d6a
Jop Middelkamp <middelkamp@live.nl>
2021-12-07 13:24:56 +0100

    Improved delta offset unit tests

--- supercharged/test/offset/delta_offset_test.dart
+++ supercharged/test/offset/delta_offset_test.dart
@@ -2,44 +2,48 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:supercharged/supercharged.dart';
 
 void main() {
-  test('Construct by left constructor', () {
-    const offset = DeltaOffset.left(5);
-    expect(offset.dx, -5);
-    expect(offset.dy, 0);
-  });
-  test('Construct by top constructor', () {
-    const offset = DeltaOffset.top(5);
-    expect(offset.dx, 0);
-    expect(offset.dy, -5);
-  });
-  test('Construct by right constructor', () {
-    const offset = DeltaOffset.right(5);
-    expect(offset.dx, 5);
-    expect(offset.dy, 0);
-  });
-  test('Construct by bottom constructor', () {
-    const offset = DeltaOffset.bottom(5);
-    expect(offset.dx, 0);
-    expect(offset.dy, 5);
-  });
-  test('Construct by leftBottom constructor', () {
-    const offset = DeltaOffset.leftBottom(5, 5);
-    expect(offset.dx, -5);
-    expect(offset.dy, 5);
-  });
-  test('Construct by leftTop constructor', () {
-    const offset = DeltaOffset.leftTop(5, 5);
-    expect(offset.dx, -5);
-    expect(offset.dy, -5);
-  });
-  test('Construct by rightBottom constructor', () {
-    const offset = DeltaOffset.rightBottom(5, 5);
-    expect(offset.dx, 5);
-    expect(offset.dy, 5);
-  });
-  test('Construct by rightTop constructor', () {
-    const offset = DeltaOffset.rightTop(5, 5);
-    expect(offset.dx, 5);
-    expect(offset.dy, -5);
+  group('delta offset', () {
+    group('constructor', () {
+      test('named left', () {
+        const offset = DeltaOffset.left(5);
+        expect(offset.dx, -5);
+        expect(offset.dy, 0);
+      });
+      test('named top', () {
+        const offset = DeltaOffset.top(5);
+        expect(offset.dx, 0);
+        expect(offset.dy, -5);
+      });
+      test('named right', () {
+        const offset = DeltaOffset.right(5);
+        expect(offset.dx, 5);
+        expect(offset.dy, 0);
+      });
+      test('named bottom', () {
+        const offset = DeltaOffset.bottom(5);
+        expect(offset.dx, 0);
+        expect(offset.dy, 5);
+      });
+      test('named leftBottom', () {
+        const offset = DeltaOffset.leftBottom(5, 5);
+        expect(offset.dx, -5);
+        expect(offset.dy, 5);
+      });
+      test('named leftTop', () {
+        const offset = DeltaOffset.leftTop(5, 5);
+        expect(offset.dx, -5);
+        expect(offset.dy, -5);
+      });
+      test('named rightBottom', () {
+        const offset = DeltaOffset.rightBottom(5, 5);
+        expect(offset.dx, 5);
+        expect(offset.dy, 5);
+      });
+      test('named rightTop', () {
+        const offset = DeltaOffset.rightTop(5, 5);
+        expect(offset.dx, 5);
+        expect(offset.dy, -5);
+      });
+    });
   });
 }
