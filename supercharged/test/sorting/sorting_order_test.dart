a68bca2d9dd5621fe3dc2e2f17ebcc540b3f0815
Jop Middelkamp <middelkamp@live.nl>
2021-12-07 12:29:00 +0100

    Sorting order test improvements

--- supercharged/test/sorting/sorting_order_test.dart
+++ supercharged/test/sorting/sorting_order_test.dart
@@ -2,27 +2,33 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:supercharged/supercharged.dart';
 
 void main() {
-  test('sortAsc extension method', () {
-    final list = [1, 3, 2, 4]..sortAsc();
+  group('sorting order', () {
+    group('extension method', () {
+      test('sortAsc', () {
+        final list = [1, 3, 2, 4]..sortAsc();
 
-    expect(list.join(','), '1,2,3,4');
-  });
+        expect(list.join(','), '1,2,3,4');
+      });
 
-  test('sortDesc extension method', () {
-    final list = [1, 3, 2, 4]..sortDesc();
+      test('sortDesc', () {
+        final list = [1, 3, 2, 4]..sortDesc();
 
-    expect(list.join(','), '4,3,2,1');
-  });
+        expect(list.join(','), '4,3,2,1');
+      });
+    });
 
-  test('sortAsc function', () {
-    final list = [1, 3, 2, 4]..sort((a, b) => sortAsc(a, b));
+    group('function', () {
+      test('sortAsc', () {
+        final list = [1, 3, 2, 4]..sort((a, b) => sortAsc(a, b));
 
-    expect(list.join(','), '1,2,3,4');
-  });
+        expect(list.join(','), '1,2,3,4');
+      });
 
-  test('sortDesc function', () {
-    final list = [1, 3, 2, 4]..sort((a, b) => sortDesc(a, b));
+      test('sortDesc', () {
+        final list = [1, 3, 2, 4]..sort((a, b) => sortDesc(a, b));
 
-    expect(list.join(','), '4,3,2,1');
+        expect(list.join(','), '4,3,2,1');
+      });
+    });
   });
 }
