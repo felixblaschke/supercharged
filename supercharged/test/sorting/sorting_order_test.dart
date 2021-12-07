1c06b949ec31e7535940b849519d53762f0cbea2
Jop Middelkamp <middelkamp@live.nl>
2021-12-07 12:22:47 +0100

    Added some sort order helper extension methods

--- supercharged/test/sorting/sorting_order_test.dart
+++ supercharged/test/sorting/sorting_order_test.dart
@@ -2,14 +2,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:supercharged/supercharged.dart';
 
 void main() {
-  test('sortAsc', () {
-    final list = [1, 3, 2, 4]..sort(sortAsc);
+  test('sortAsc extension method', () {
+    final list = [1, 3, 2, 4]..sortAsc();
 
     expect(list.join(','), '1,2,3,4');
   });
 
-  test('sortDesc', () {
-    final list = [1, 3, 2, 4]..sort(sortDesc);
+  test('sortDesc extension method', () {
+    final list = [1, 3, 2, 4]..sortDesc();
+
+    expect(list.join(','), '4,3,2,1');
+  });
+
+  test('sortAsc function', () {
+    final list = [1, 3, 2, 4]..sort((a, b) => sortAsc(a, b));
+
+    expect(list.join(','), '1,2,3,4');
+  });
+
+  test('sortDesc function', () {
+    final list = [1, 3, 2, 4]..sort((a, b) => sortDesc(a, b));
 
     expect(list.join(','), '4,3,2,1');
   });
